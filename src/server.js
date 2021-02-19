8603ba0cf64b077d4da7df7944cc890d4316a87a
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-19 10:05:22 +0000

    converted deezer api from FE to BE

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const catchAllHandler = require("./errorHandler");
+
+const deezerRouter = require("./deezer-crud");
 
 const server = express();
 const port = process.env.PORT || 3003;
@@ -8,8 +11,12 @@ const port = process.env.PORT || 3003;
 server.use(cors());
 server.use(express.json());
 
+server.use("/deezer", deezerRouter);
+
+server.use(catchAllHandler);
+
 mongoose
-  .connect(process.env.MONGO_ATLAS, {
+  .connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
